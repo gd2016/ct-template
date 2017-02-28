62f5c6554f1e9309363fbb8a3eecb9595f4ab03e
RoamIn <ivill@live.com>
2017-02-28 11:11:32 +0800

    URL case insensitive

--- template/mock/define.js
+++ template/mock/define.js
@@ -1,22 +1,24 @@
 var interFaces = new Map();
-var fs=require('fs');
-var files=fs.readdirSync(__dirname+'/data');
-files.map((file)=>{
-  var data=require(__dirname+'/data/'+file);
-  Object.keys(data).map((key)=>{
-    interFaces.set(key,data[key]);
+var fs = require('fs');
+var files = fs.readdirSync(__dirname + '/data');
+
+files.map((file) => {
+  var data = require(__dirname + '/data/' + file);
+  Object.keys(data).map((key) => {
+    interFaces.set(key.toLowerCase(), data[key]);
   })
 });
+
 module.exports = {
   define: {
     isProxy: true,
-    matchPath:/\/api/,
+    matchPath: /\/api/,
     domains: {
       test1: "http://tcysystinker.admin.ct108.org:1505"
     }
   },
   interFaces: interFaces,
   getInterFace(key){
-    return interFaces.get(key);
+    return interFaces.get(key.toLowerCase());
   }
 };
--- template/mock/index.js
+++ template/mock/index.js
@@ -1,30 +1,32 @@
 var proxy = require("express-http-proxy");
 var mockData = require("./define");
 var apiProxy = function () {
-    if (mockData.define.isProxy) {
-        return proxy(mockData.define.domains.test1, {
-            forwardPath: function (req, res) {
-                return req._parsedUrl.path
-            }
+  if (mockData.define.isProxy) {
+    return proxy(mockData.define.domains.test1, {
+      forwardPath: function (req, res) {
+        return req._parsedUrl.path
+      }
+    });
+  } else {
+    return function (req, res, next) {
+      if (req.baseUrl) {
+        res.json(mockData.getInterFace(req.baseUrl));
+      } else {
+        res.json({
+          "msg": "nodata"
         });
-    }else{
-      return function (req, res, next) {
-        if (req.baseUrl) {
-          res.json(mockData.getInterFace(req.baseUrl));
-        } else {
-          res.json({
-            "msg": "nodata"
-          });
-        }
-      };
-    }
+      }
+    };
+  }
 }();
+
 module.exports = function (app) {
-    //模拟数据
-  if(mockData.define.isProxy && mockData.define.matchPath!==''){
-    app.use(mockData.define.matchPath,apiProxy);
-  }else{
+  //模拟数据
+  if (mockData.define.isProxy && mockData.define.matchPath !== '') {
+    app.use(mockData.define.matchPath, apiProxy);
+  } else {
     var keys = mockData.interFaces.keys();  //遍历Key
+
     for (var key of keys) {
       app.use(key, apiProxy);
     }
