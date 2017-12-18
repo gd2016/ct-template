01e3ad9b9bc8b963533ef7171ba66b84c9cf0061
skysunshine99 <skysunshine99@126.com>
2017-12-18 19:53:28 +0800

    test

--- meta.js
+++ meta.js
@@ -82,11 +82,15 @@ module.exports = {
   "filters": {
     ".eslintrc.js": "lint",
     ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
-    "test/unit/**/*": "unit",
-    "build/webpack.test.conf.js": "unit",
-    "test/e2e/**/*": "e2e",
-    "src/router/**/*": "router"
+    //"config/test.env.js": "unit || e2e",
+    //"test/unit/**/*": "unit",
+    //"build/webpack.test.conf.js": "unit",
+    //"test/e2e/**/*": "e2e",
+      "config/test.env.js": false,
+      "test/unit/**/*": false,
+      "build/webpack.test.conf.js": false,
+      "test/e2e/**/*": false,
+    "src/router/**/*": false
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
