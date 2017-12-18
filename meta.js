87c7ceb46bb2fefadeb4cab07d15fcbbbb9abc29
skysunshine99 <skysunshine99@126.com>
2017-12-18 20:25:06 +0800

    test

--- meta.js
+++ meta.js
@@ -43,33 +43,12 @@ module.exports = {
         "router": {
             "type": "confirm",
             "message": "Install vue-router?"
-        }
-        //"lint": {
-        //    "type": "confirm",
-        //    "message": "Use ESLint to lint your code?"
-        //},
-        //"lintConfig": {
-        //    "when": "lint",
-        //    "type": "list",
-        //    "message": "Pick an ESLint preset",
-        //    "choices": [
-        //        {
-        //            "name": "Standard (https://github.com/feross/standard)",
-        //            "value": "standard",
-        //            "short": "Standard"
-        //        },
-        //        {
-        //            "name": "AirBNB (https://github.com/airbnb/javascript)",
-        //            "value": "airbnb",
-        //            "short": "AirBNB"
-        //        },
-        //        {
-        //            "name": "none (configure it yourself)",
-        //            "value": "none",
-        //            "short": "none"
-        //        }
-        //    ]
-        //}
+        },
+        "lint": {
+            "type": "confirm",
+            "message": "Use ESLint to lint your code?"
+        },
+        "lintConfig": "none"
     },
     "filters": {
         ".eslintrc.js": true,
