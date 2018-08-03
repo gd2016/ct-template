7b4ac00680410b92510030747177d197f6f4a22e
gd <371049740@qq.com>
2018-08-03 09:39:35 +0800

    Update app.vue

--- template/src/js/app/main/component/app.vue
+++ template/src/js/app/main/component/app.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container-fluid">{{#searchOperation}}
         <ct-form  searchForm :loading="loading" @search="search">{{#each searchItems}}
-            <form-item v-model="searchInfo.Id" type="text" label="ID"></form-item>{{#/each}}
+            <form-item v-model="searchInfo.Id" type="text" label="ID"></form-item>{{/each}}
             <form-item v-model="searchInfo.Type" type="select" defaultSelect :selectList="typeList" label="类型"></form-item>  
         </ct-form>{{/searchOperation}}
         <div class="clearfix mb10">
