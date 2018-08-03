e8e8aa4c4f002a67d516f44150311b0ba69ea893
guoding <guoding@ct108.com>
2018-08-03 14:45:47 +0800

    .

--- meta.js
+++ meta.js
@@ -18,12 +18,20 @@ module.exports = {
             return options.inverse(this);
         },
         "foreach": function (v1, v2, options) {
-            var templates = null;
-            for (let index = 0; index < v1; index++) {
-                templates+=options.fn(this);
-            }
-            return templates;
-        }
+            console.log(v1,v2,options);
+              if (v1 || v2) {
+                  return options.fn(this);
+              }
+  
+              return options.inverse(this);
+          }
+        // "": function (v1, v2, options) {
+        //     var templates = null;
+        //     for (let index = 0; index < v1; index++) {
+        //         templates+=options.fn(this);
+        //     }
+        //     return templates;
+        // }
     },
     "prompts": {
         "projectType": {
