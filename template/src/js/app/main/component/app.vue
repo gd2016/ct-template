7b439d638ea2b01558629e38fa6ff7a0337a0ec6
guoding <guoding@ct108.com>
2018-08-07 18:21:24 +0800

    .

--- template/src/js/app/main/component/app.vue
+++ template/src/js/app/main/component/app.vue
@@ -2,12 +2,9 @@
     <div class="container-fluid">{{#operation.search}}
         <ct-form  searchForm :loading="loading" @search="search">
             {{#each_item searchCount "search_"}}
-            {{#if_is @type 'select'}}
-            <form-item v-model="searchInfo.{{@field}}" type="{{@type}}" label="{{@label}}" :list="typeList" defaultSelect></form-item>
-            {{else if_is @type 'autoComplete'}}
-            <form-item v-model="searchInfo.{{@field}}" type="{{@type}}" label="{{@label}}" :list="typeList" :matchKeys="['key','val']" :keys="['key','val']" :showKeys="['key','val']"></form-item>
-            {{else}}
-            <form-item v-model="searchInfo.{{@field}}" type="{{@type}}" label="{{@label}}"></form-item>
+            {{#if_is @type 'select'}}<form-item v-model="searchInfo.{{@field}}" type="{{@type}}" label="{{@label}}" :list="typeList" defaultSelect></form-item>
+            {{else if_is @type 'autoComplete'}}<form-item v-model="searchInfo.{{@field}}" type="{{@type}}" label="{{@label}}" :list="typeList" :matchKeys="['key','val']" :keys="['key','val']" :showKeys="['key','val']"></form-item>
+            {{else}}<form-item v-model="searchInfo.{{@field}}" type="{{@type}}" label="{{@label}}"></form-item>
             {{/if_is}}
             {{/each_item}}
         </ct-form>{{/operation.search}}
