fe244f41ed7cac8dcc93d7001af1df1697975f4a
guoding <guoding@ct108.com>
2018-08-07 14:47:48 +0800

    .

--- template/src/js/module/common/rules.js
+++ template/src/js/module/common/rules.js
@@ -1,12 +1,12 @@
 {{#operation.edit}}export const editRules = {
-    {{#each_item editCount "edit_"}}{{@field}}: [{required: true, message: '{{@label}}不能为空'}],
-        {{/each_item}}
+    // {{#each_item editCount "edit_"}}{{@field}}: [{required: true, message: '{{@label}}不能为空'}],
+    //     {{/each_item}}
 };
 {{/operation.edit}}
 
 {{#operation.add}}export const addRules = {
-    {{#each_item addCount "add_"}}{{@field}}: [{required: true, message: '{{@label}}不能为空'}],
-        {{/each_item}}
+    // {{#each_item addCount "add_"}}{{@field}}: [{required: true, message: '{{@label}}不能为空'}],
+    //     {{/each_item}}
 }
 ;
 {{/operation.add}}
