0efb0118eb18c026578196d6a3b456b07c484477
guoding <guoding@ct108.com>
2018-08-15 20:05:52 +0800

    .

--- template/src/js/app/main/component/app.vue
+++ template/src/js/app/main/component/app.vue
@@ -4,7 +4,7 @@
             {{#each_search searchInfo}}
             {{#if_is @type 'select'}}<form-item v-model="searchInfo.{{@field}}" type="{{@type}}" label="{{@label}}" :list="typeList" defaultSelect></form-item>{{else if_is @type 'autoComplete'}}<form-item v-model="searchInfo.{{@field}}" type="{{@type}}" label="{{@label}}" :list="typeList" :matchKeys="['key','val']" :keys="['key','val']" :showKeys="['key','val']"></form-item>{{else}}<form-item v-model="searchInfo.{{@field}}" type="{{@type}}" label="{{@label}}"></form-item>{{/if_is}}
             {{/each_search}}
-        </ct-form>{{/operation.search}}{{if_or operation.export operation.add}}
+        </ct-form>{{/operation.search}}{{#if_or operation.export operation.add}}
         <div class="clearfix mb10">{{#operation.export}}
             <export-btn :searchInfo="searchInfo" :url="exportUrl"></export-btn>{{/operation.export}}{{#operation.add}}
             <button @click="add" type="button" class="btn btn-sm btn-primary pull-right"><span class="glyphicon glyphicon-plus"></span>添加</button>{{/operation.add}}
